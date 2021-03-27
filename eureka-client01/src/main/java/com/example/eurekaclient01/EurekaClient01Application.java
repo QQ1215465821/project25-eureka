338a530f53cc331693ac48c8ac731795ae8a8670
刘鹏 <46567390+QQ1215465821@users.noreply.github.com>
2021-03-27 16:22:53 +0800

    2021年3月27日16:22:44

--- eureka-client01/src/main/java/com/example/eurekaclient01/EurekaClient01Application.java
+++ eureka-client01/src/main/java/com/example/eurekaclient01/EurekaClient01Application.java
@@ -13,3 +13,24 @@ public class EurekaClient01Application {
     }
 
 }
+
+/*
+docker run -it -p 8081:8081 --name eureka_client_8081 lam/eureka-client01 --server.port=8081 --eureka.instance.hostname=host01
+ */
+/*
+镜像创建容器配置
+<component name="ProjectRunConfigurationManager">
+  <configuration default="false" name="Docker镜像配置2" type="docker-deploy" factoryName="docker-image" server-name="Docker">
+    <deployment type="docker-image">
+      <settings>
+        <option name="imageTag" value="lam/eureka-client01:latest" />
+        <option name="command" value="--server.port=8082 --eureka.instance.hostname=host02" />
+        <option name="containerName" value="eureka_client_8082" />
+        <option name="entrypoint" value="" />
+        <option name="commandLineOptions" value="-it -p 8082:8082" />
+      </settings>
+    </deployment>
+    <method v="2" />
+  </configuration>
+</component>
+ */
